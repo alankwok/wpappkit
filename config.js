cf338442e75d1bd7c5f07113267348a1ff73d82c
alankwok <alankwok@yahoo.com>
2015-09-28 06:34:58 +0800

    20150928

---
+++ config.js
@@ -0,0 +1,17 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'hktopblogger',
+		wp_ws_url : 'http://alankwok.hopto.org/wp-appkit-api/hktopblogger',
+		theme : 'wp-appkit-themes-master',
+		version : '1.0.0',
+		app_title : 'HKTopblogger',
+		debug_mode : 'off',
+		auth_key : 'u1*kOH#._{-Om8)=.?i2h_me#aWZM(~9J8b%uK1bM)BwIQ6Nn<2.j#j!MhqW6QL@',
+		options : {"refresh_interval":0},
+		addons : []
+	};
+
+});
